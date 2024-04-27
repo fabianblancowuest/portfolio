8a3bb56923509cc9090e0db3a14f246eb05de27d
Fabian Blanco Wuest <fabianblancowuest@gmail.com>
2024-04-27 10:40:24 -0300

    alerts added

--- app.js
+++ app.js
@@ -2,6 +2,25 @@ document.addEventListener("DOMContentLoaded", function () {
 	const subtitle = document.querySelector(".subtitle");
 	const text = subtitle.textContent;
 
+	// Change image logo dinamically
+	const images = ["img/codigo.png", "img/programacion.png"]; // Rutas de las imágenes del logo
+	const logo = document.getElementById("logo-img");
+	let index = 0;
+
+	setInterval(function () {
+		logo.src = images[index];
+		index = (index + 1) % images.length;
+	}, 3000);
+
+	// let ind = 0;
+	// const logoFooter = document.getElementById("logo-img-footer");
+
+	// setInterval(function () {
+	// 	logoFooter.src = images[index];
+	// 	ind = (ind + 1) % images.length;
+	// }, 3000);
+
+	// typewriter effect
 	function typeWriterEffect() {
 		subtitle.textContent = "";
 
@@ -16,6 +35,7 @@ document.addEventListener("DOMContentLoaded", function () {
 			}
 		}
 
+		// eraser effect
 		function erase() {
 			if (subtitle.textContent.length > 0) {
 				subtitle.textContent = text.substring(
@@ -33,3 +53,36 @@ document.addEventListener("DOMContentLoaded", function () {
 
 	typeWriterEffect(); // Iniciar el efecto
 });
+
+const logo = document.getElementById("logo");
+let index = 0;
+
+logo.addEventListener("click", () => {
+	// alert("Hola");
+	const phrases = [
+		'"I’m not a great programmer; I’m just a good programmer with great habits. ― Kent Beck"',
+		'"You might not think that programmers are artists, but programming is an extremely creative profession. It’s logic-based creativity. – John Romero"',
+		'"Programming is the art of algorithm design and the craft of debugging errant code. – Ellen Ullman"',
+		'"Any fool can write code that a computer can understand. Good programmers write code that humans can understand. ― Martin Fowler"',
+		'"If we want users to like our software, we should design it to behave like a likable person."  – Alan Cooper',
+		'"Code is like humor. When you have to explain it, it’s bad." – Cory House',
+		'"Programming is the art of telling another human being what one wants the computer to do. ― Donald Ervin Knuth"',
+		'"Confusion is part of programming. ― Felienne Hermans"',
+		'"Web development is difficult, only then it is fun to do. You just have to set your standards. If it were to be easy, would anyone do it? ― Olawale Daniel"',
+		'"When I wrote this code, only God and I understood what I did. Now only God knows."  – Anonymous',
+		'"You might not think that programmers are artists, but programming is an extremely creative profession. It’s logic-based creativity." – John Romero',
+		'"Software and cathedrals are much the same — first we build them, then we pray."',
+	];
+
+	if (index < phrases.length) {
+		Swal.fire({
+			title: phrases[index],
+			confirmButtonColor: "rgb(13,202,240)",
+		});
+		index++;
+	} else {
+		index = 0;
+	}
+});
+
+document.addEventListener("DOMContentLoaded", function () {});
---
+++ email.js
@@ -0,0 +1,30 @@
+(function () {
+	// https://dashboard.emailjs.com/admin/account
+	emailjs.init({
+		publicKey: "Qj-2BHcvaJJZlfg9O",
+	});
+})();
+
+window.onload = function () {
+	document
+		.getElementById("contact-form")
+		.addEventListener("submit", function (event) {
+			event.preventDefault();
+			// these IDs from the previous steps
+			emailjs.sendForm("service_hre43nq", "template_it5kkwt", this).then(
+				(response) => {
+					console.log("SUCCESS!", response.status, response.text);
+					// Restart form fields
+					document.getElementById("contact-form").reset();
+
+					var modalElement = document.getElementById("contactModal");
+					var modal = bootstrap.Modal.getInstance(modalElement);
+					// Close form
+					modal.hide();
+				},
+				(error) => {
+					console.log("FAILED...", error);
+				},
+			);
+		});
+};
