a1a7f0ccc0f746b15b86497cc7b6a67043ff9a36
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2024-07-14 07:07:06 -0300

    webpack added

--- js/index.js
+++ js/index.js
@@ -1,20 +1,6 @@
-import { changeLanguage } from "./change_language.js";
-
-document.addEventListener("DOMContentLoaded", () => {
-	const languageSelector = document.getElementById("language-selector");
-
-	const html = document.querySelector("html");
-
-	languageSelector.addEventListener("change", (event) => {
-		const selectedLanguage = event.target.value;
-		changeLanguage(selectedLanguage);
-		// Change language atributte
-		html.lang = selectedLanguage;
-		console.log(html.lang);
-	});
-
-	// Establece el idioma inicial
-
-	changeLanguage(languageSelector.value);
-	console.log("select language");
-});
+import "./app.js";
+import "./change_language.js";
+import "./email.js";
+import "./resize.js";
+import "./set_language.js";
+import "./typewriter.js";
