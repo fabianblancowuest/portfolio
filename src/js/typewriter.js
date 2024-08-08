cf0fc4b642702df87278ae6fe5347b93e67ab21f
Fabián Blanco Wuest <fabianblancowuest@gmail.com>
2024-08-08 05:21:55 -0300

    improving the interface

--- src/js/typewriter.js
+++ src/js/typewriter.js
@@ -12,13 +12,19 @@ function typeWriterEffect(phrases) {
 	const selectedPhrases = phrases;
 	let currentIndex = 0;
 
+	// Crear el elemento del cursor
+	const cursor = document.createElement("span");
+	cursor.classList.add("typing-cursor");
+	subtitle.appendChild(cursor);
+
 	function type() {
 		const text = selectedPhrases[currentIndex];
 		let i = 0;
 
 		function typeCharacter() {
 			if (i < text.length) {
-				subtitle.textContent += text.charAt(i);
+				subtitle.textContent = text.substring(0, i + 1); // Actualiza el texto
+				subtitle.appendChild(cursor); // Asegura que el cursor esté al final
 				i++;
 				typingTimeout = setTimeout(typeCharacter, 100); // Velocidad de escritura en milisegundos
 			} else {
@@ -36,6 +42,7 @@ function typeWriterEffect(phrases) {
 		function eraseCharacter() {
 			if (i >= 0) {
 				subtitle.textContent = text.substring(0, i);
+				subtitle.appendChild(cursor); // Asegura que el cursor esté al final
 				i--;
 				erasingTimeout = setTimeout(eraseCharacter, 50); // Velocidad de borrado en milisegundos
 			} else {
